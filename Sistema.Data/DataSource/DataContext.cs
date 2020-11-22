b1bf1335734511344a0cee354348a02141e1428d
Sandro Arpi <sandro.teste72@gmail.com>
2020-11-22 13:46:20 -0300

    Criando classe de contexto

---
+++ Sistema.Data/DataSource/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Sistema.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Data.DataSource
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            : base(ConfigurationManager.ConnectionStrings["aula"].ConnectionString)
+        {
+
+        }
+
+        public DbSet<Funcionario> Funcionario { get; set; }
+        public DbSet<Endereco> Endereco { get; set; }
+        public DbSet<Cargo> Cargo { get; set; }
+    }
+}
